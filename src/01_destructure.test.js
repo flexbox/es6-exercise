e46676f7869e0f147995a2af91ebfb642c451875
David Leuliette <dleuliette@gmail.com>
2018-12-03 10:22:13 +0000

    fix part !

--- src/01_destructure.test.js
+++ src/01_destructure.test.js
@@ -3,21 +3,21 @@
 
 describe('destructuring arrays makes shorter code', () => {
   it('extract value from array, e.g. extract 0 into x like so `let [x] = [0]`', () => {
-    let firstValue = [1]
+    let [firstValue] = [1]
 
     expect(firstValue).toEqual(1)
   })
 
   it('leading commas', () => {
     const all = ['ax', 'why', 'zet']
-    const [, z] = all
+    const [x, y, z] = all
 
     expect(z).toEqual('zet')
   })
 
   it('extract from nested arrays', () => {
     const user = [['Some', 'One'], 23]
-    const [firstName, surname, age] = user
+    const [[firstName, surname], age] = user
 
     const expected = 'Some One = 23 years'
     expect(`${firstName} ${surname} = ${age} years`).toEqual(expected)
@@ -25,14 +25,13 @@ describe('destructuring arrays makes shorter code', () => {
 
   it('chained assignments', () => {
     let c, d
-    let a,
-      b = ([c, d] = [1, 2])
+    let [a, b] = ([c, d] = [1, 2])
 
     expect([a, b, c, d]).toEqual([1, 2, 1, 2])
   })
 
   it('in for-of loop', () => {
-    for (var [a, b] of [[0, 1, 2]]) {
+    for (var [z, a, b] of [[0, 1, 2]]) {
     }
     expect([a, b]).toEqual([1, 2])
   })
